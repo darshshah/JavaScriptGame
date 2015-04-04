577437e277712e712678a4cde1a52e2c66d17ee0
Darsh Shah <darshs@weh5201-b3.weh.andrew.cmu.edu>
2015-04-03 20:47:53 -0400

    added the board initialization function

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
+	var rows;
+	var cols;
+
+	var boardColors = []
+
 ;(function() {
     function play(e) {
+    	e.preventDefault();
+    	rows = $('#rows').val();
+    	cols = $('#cols').val();
+
+    	$('#submit').detach();
+
+    	alert(rows*cols);
+
+    	for (var i=0; i<rows; i++) 
+    	{
+    		var curTileRow = $('<div class="tile-row"></div>');
+	        $('#tile-wrapper').append(curTileRow);
+
+	        boardColors[i] = [];
+
+    		for (var j=0; j<cols; j++)
+    		{
+    			var curTileCol = $('<div class="tile"></div>');
+    			setColor(i, j, 'blue');	
+    		}
+
+    		getTile(i, j).click((function(r, c) {
+            return function(e) {
+              move(e, r, c);
+            }
+          })(i, j));
+
+    	}
 
     }
 
